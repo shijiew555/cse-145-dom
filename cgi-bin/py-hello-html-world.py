46021b0c3fd151a3fa23715828537b4f36d904a2
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-20 23:05:08 -0700

    Create py-hello-html-world.py

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+print("Cache-Control: no-cache\n")
+print("Content-type: text/html\n\n")
+print("<html><head><title>Hello CGI World</title></head>\
+	     <body><h1 align=center>Hello HTML World</h1>\
+  	   <hr/>\n")
+print("Hello World<br/>\n")
+print("This program was generated at: %s\n<br/>", time.time())
+print("Your current IP address is: %s<br/>", os.environ["REMOTE_ADDR"])
+ 
+// Print HTML footer
+print("</body></html>")
