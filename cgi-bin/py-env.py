d8b87134ea12814059a37911b90afdb870b729ae
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-21 19:59:49 -0700

    Update py-env.py

--- cgi-bin/py-env.py
+++ cgi-bin/py-env.py
@@ -4,7 +4,6 @@ import os
 print("Content-type: text/html\r\n\r\n")
 
 print("<html><head><title>Environment Variables</title></head><body><h1 align=center>Environment Variables</h1><hr/>")
-print("<html><head><title>Environment Variables</title></head><body><h1 align=center>Environment Variables</h1><hr/>")
 
 for e in os.environ:
   print(e+"<br/>")
