cadb544ee750923e72997fdcb75b51195a24e35b
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-21 19:47:31 -0700

    Update py-hello-html-world.py

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -6,7 +6,7 @@ import os
 print("Content-type:text/html\r\n\r\n")
 print("<html><head><title>Hello CGI World</title></head> <body><h1 align=center>Hello HTML World</h1> <hr/>\n")
 print("Hello World<br/>\n")
-print("This program was generated at: %s\n<br/>" % time.time())
+print("This program was generated at: %s\n<br/>" % time.ctime())
 print("Your current IP address is: %s<br/>" % os.environ["REMOTE_ADDR"])
 
 print("</body></html>")
