cb7a70a0e1d3d538d390fdf0ab1538c70d159cb2
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-21 19:41:42 -0700

    Update py-hello-json-world.py

--- cgi-bin/py-hello-json-world.py
+++ cgi-bin/py-hello-json-world.py
@@ -4,4 +4,4 @@ import time
 import os
 
 print("Content-type: application/json\r\n\r\n")
-print("{\"message\":\"Hello World\",\"date\":\"%s\",\"currentIP\":\"%s\"}" % ("123", "123"))
+print("{\"message\":\"Hello World\",\"date\":\"%s\",\"currentIP\":\"%s\"}" % (time.date(), os.environ["REMOTE_ADDR"))
