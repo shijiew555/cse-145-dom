6e59472eed92f13bdb358ef8ff00b98aa6624ff4
ShijieWang <shijiew21@gmail.com>
2022-04-22 16:54:51 +0000

    add py-post

---
+++ cgi-bin/py-post-echo.py
@@ -0,0 +1,15 @@
+"import sys"
+
+print("Content-type: text/html")
+print("<html><head><title>POST Message Body</title></head><body><h1 align=center>POST Message Body</h1><hr/>")
+
+"""
+print("Message Body: ")
+
+for line in sys.stdin:
+  print(line)
+
+print("<br/>")
+"""
+print("</body>")
+print("</html>")
