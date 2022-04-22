420c243197017407d0431426cafa59952f032340
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-21 19:35:32 -0700

    remove semicolon

--- cgi-bin/py-hello-json-world.py
+++ cgi-bin/py-hello-json-world.py
@@ -3,5 +3,5 @@
 import time
 import os
 
-print("Content-type: application/json");
-print("{\"message\":\"Hello World\",\"date\":\"%s\",\"currentIP\":\"%s\"}" % (time.time(), os.environ["REMOTE_ADDR"]));
+print("Content-type: application/json")
+print("{\"message\":\"Hello World\",\"date\":\"%s\",\"currentIP\":\"%s\"}" % (time.time(), os.environ["REMOTE_ADDR"]))
