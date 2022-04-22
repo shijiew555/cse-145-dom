217948ea03159a575c3763b6229ec9eb04ed27e2
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-22 10:08:36 -0700

    Update py-general-request-echo.py

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -3,20 +3,20 @@
 import os
 import sys
 
-print("Cache-Control: no-cache\n")
 print("Content-type: text/html\n\n")
 print("<html><head><title>General Request Echo</title></head><body><h1 align=center>General Request Echo</h1><hr/>")
 
 // Get environment vars
 print("<table>\n")
-print("<tr><td>Protocol:</td><td>%s</td></tr>\n", os.environ["SERVER_PROTOCOL"])
-print("<tr><td>Method:</td><td>%s</td></tr>\n", os.environ["REQUEST_METHOD"])
+print("<tr><td>Protocol:</td><td>%s</td></tr>", os.environ["SERVER_PROTOCOL"])
+print("<tr><td>Method:</td><td>%s</td></tr>", os.environ["REQUEST_METHOD"])
+
 print("<tr><td>Message Body:</td><td>")
 
 for line in sys.stdin:
   print(line)
   
-print("</td></tr>\n")
+print("</td></tr>")
 
 // Print HTML footer
 print("</body>");
