41af718baefffcf4827e57950dcb48ffe2ffa1ed
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-25 09:15:58 -0700

    Update py-sessions-1.py

--- cgi-bin/py-sessions-1.py
+++ cgi-bin/py-sessions-1.py
@@ -19,45 +19,41 @@ name = ""
 if username[0] == 'u':
   name = username[0:9]
 
-  # Set the cookie using a header, add extra \n to end headers
-  if len(name) > 0:
-    print("Content-type: text/html\n")
-    print("Set-Cookie: %s\n\n" % name)
-  else
-    print("Content-type: text/html\n\n")
+# Set the cookie using a header, add extra \n to end headers
+if len(name) > 0:
+  print("Content-type: text/html\n")
+  print("Set-Cookie: %s\n\n" % name)
+else:
+  print("Content-type: text/html\n\n")
 
-  # Body - HTML
-  print("<html>")
-  print("<head><title>Python Sessions</title></head>\n")
-  print("<body>")
-  print("<h1>Python Sessions Page 1</h1>")
-  print("<table>")
+# Body - HTML
+print("<html>")
+print("<head><title>Python Sessions</title></head>")
+print("<body>")
+print("<h1>Python Sessions Page 1</h1>")
+print("<table>")
 
-  # First check for new Cookie, then Check for old Cookie
-  if len(name) > 0:
-  {
-    print("<tr><td>Cookie:</td><td>%s</td></tr>\n" % name)
-  }
-  elif (os.environ["HTTP_COOKIE"] != None && os.environ["HTTP_COOKIE"] == "destroyed"):
-  {
-    print("<tr><td>Cookie:</td><td>%s</td></tr>\n" % os.environ["HTTP_COOKIE"])
-  }
-  else
-    print("<tr><td>Cookie:</td><td>None</td></tr>\n")
+# First check for new Cookie, then Check for old Cookie
+if len(name) > 0:
+  print("<tr><td>Cookie:</td><td>%s</td></tr>" % name)
+elif (os.environ["HTTP_COOKIE"] != None and os.environ["HTTP_COOKIE"] == "destroyed"):
+  print("<tr><td>Cookie:</td><td>%s</td></tr>" % os.environ["HTTP_COOKIE"])
+else:
+  print("<tr><td>Cookie:</td><td>None</td></tr>")
 
-  print("</table>")
+print("</table>")
 
-  # Links for other pages
-  print("<br />")
-  print("<a href=\"/cgi-bin/c-sessions-2.cgi\">Session Page 2</a>")
-  print("<br />")
-  print("<a href=\"/c-cgiform.html\">C CGI Form</a>")
-  print("<br /><br />")
+# Links for other pages
+print("<br />")
+print("<a href=\"/cgi-bin/py-sessions-2.pt\">Session Page 2</a>")
+print("<br />")
+print("<a href=\"/py-cgiform.html\">Py CGI Form</a>")
+print("<br /><br />")
 
-  # Destroy Cookie button
-  print("<form action=\"/cgi-bin/c-destroy-session.cgi\" method=\"get\">")
-  print("<button type=\"submit\">Destroy Session</button>")
-  print("</form>");
+# Destroy Cookie button
+print("<form action=\"/cgi-bin/c-destroy-session.cgi\" method=\"get\">")
+print("<button type=\"submit\">Destroy Session</button>")
+print("</form>");
 
-  print("</body>")
-  print("</html>")
+print("</body>")
+print("</html>")
