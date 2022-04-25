2da39504353d4709e8694452e0766cdda23512bc
ShijieWang <shijiew21@gmail.com>
2022-04-25 06:22:05 +0000

    general-request working

--- cgi-bin/py-general-request-echo.py
+++ cgi-bin/py-general-request-echo.py
@@ -8,14 +8,17 @@ print("<html><head><title>General Request Echo</title></head><body><h1 align=cen
 
 #Get environment vars
 print("<table>")
-print("<tr><td>Protocol: </td><td>%s</td></tr>" % os.environ["SERVER_PROTOCOL"])
-print("<tr><td>Method: </td><td>%s</td></tr>" % os.environ["REQUEST_METHOD"])
+print("<tr><td>Protocol:</td><td>%s</td></tr>" % os.environ["SERVER_PROTOCOL"])
+print("<tr><td>Method:</td><td>%s</td></tr>" % os.environ["REQUEST_METHOD"])
 
-#bodyStr = ""
-#for line in sys.stdin:
-#  bodyStr = bodyStr + line
-  
-print("<tr><td>Message Body: </td><td>%s</td></tr>" % sys.stdin[0])
+bodystr = ""
+first = 0
+for line in sys.stdin:
+    if first == 0:
+        bodystr = bodystr + line
+    first = 1
+
+print("<tr><td>Message Body: </td><td>%s</td></tr>" % bodystr)
 
 print("</table>")
 #// Print HTML footer
