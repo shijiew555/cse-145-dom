38a208cb3b5eca8ac684bde6fe2288811240d801
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-25 09:21:14 -0700

    Create py-destroy-session.py

---
+++ cgi-bin/py-destroy-session.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+# Headers
+print("Cache-Control: no-cache")
+print("Set-Cookie: destroyed")
+print("Content-type: text/html\n\n")
+
+# Body - HTML
+print("<html>")
+print("<head><title>Py Session Destroyed</title></head>")
+print("<body>")
+print("<h1>Py Session Destroyed</h1>")
+
+# Links
+print("<a href=\"/cgi-bin/py-sessions-1.py\">Back to Page 1</a>")
+print("<br />")
+print("<a href=\"/cgi-bin/py-sessions-2.py\">Back to Page 2</a>")
+print("<br />")
+print("<a href=\"/py-cgiform.html\">PY CGI Form</a>")
+
+print("</body>")
+print("</html>")
