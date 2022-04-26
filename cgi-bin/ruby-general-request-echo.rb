14bc3f56e2b6bb0c48f276ba2906343702af8c31
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-25 19:25:04 -0700

    Update ruby-general-request-echo.rb

--- cgi-bin/ruby-general-request-echo.rb
+++ cgi-bin/ruby-general-request-echo.rb
@@ -7,7 +7,8 @@ puts "<html><head><title>General Request Echo</title></head><body><h1 align=cent
 puts "<table>"
 puts "<tr><td>Protocol:</td><td>%s</td></tr>" % ENV["SERVER_PROTOCOL"]
 puts "<tr><td>Method:</td><td>%s</td></tr>" % ENV["REQUEST_METHOD"]
-puts "<tr><td>Message Body: </td><td>%s</td></tr>" % STDIN.readline()
+
+puts "<tr><td>Message Body:</td><td>%s</td></tr>" % STDIN.readline()
 
 puts "</table>"
 #// Print HTML footer
