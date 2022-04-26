f9a6ee5162164f2e385a404afca02d2a4bd81c1a
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-25 18:45:47 -0700

    Update ruby-get-echo.rb

--- cgi-bin/ruby-get-echo.rb
+++ cgi-bin/ruby-get-echo.rb
@@ -4,16 +4,19 @@
 puts "Content-type: text/html\n\n"
 puts "<html><head><title>GET query string</title></head><body><h1 align=center>GET query string</h1><hr/>"
 
-puts "Raw query string: %s \n<br/><br/>" % ENV["QUERY_STRING"]
+puts "Raw query string: %s \n<br/><br/>\n" % ENV["QUERY_STRING"]
 puts "<table> Formatted Query String:"
 
-query = ENV["QUERY_STRING"]
-params = query.split("&", -1)
+params = ENV["QUERY_STRING"].split('&', -1)
 
-for p in params:
-  (pname, pval) = p.split("=")
-  puts "<tr><td> %s:</td><td> %s </td></tr>" % [pname, pval]
-  
+params.each { |p|
+  pair = p.split('=', -1)
+  puts "<tr><td> %s:</td><td> %s </td></tr>" % [pair[0], pair[1]]
+}
+
+#  puts "<tr><td> %s:</td><td> %s </td></tr>" % [pair[0], pair[1]]
+#  puts "<tr><td>sss</td><td> dsss </td></tr>"
+
 puts "</table>"
 
 puts "</body>"
