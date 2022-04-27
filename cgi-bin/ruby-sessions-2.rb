3173cde5a7c44f41b290bd8888ccbadd525fcd2c
Shijie Wang <83149651+shijiew555@users.noreply.github.com>
2022-04-26 21:18:37 -0700

    Update ruby-sessions-2.rb

--- cgi-bin/ruby-sessions-2.rb
+++ cgi-bin/ruby-sessions-2.rb
@@ -1,35 +1,46 @@
 #!/usr/bin/ruby
 
+require "cgi"
 
 # Headers
 puts "Cache-Control: no-cache"
-puts "Content-type: text/html\n"
+puts "Content-type: text/html\n\r"
+
 
 # Body - HTML
 puts "<html>"
-puts "<head><title>Py Sessions</title></head>"
+puts "<head><title>Ruby Sessions</title></head>"
 puts "<body>"
-puts "<h1>Py Sessions Page 2</h1>"
+puts "<h1>Ruby Sessions Page 2</h1>"
 puts "<table>"
 
-ck = (ENV["HTTP_COOKIE"].split(";"))[1]
-ck = ck.slice(1..(ck.length-1))
-if ENV["HTTP_COOKIE"] != None and ck != "destroyed"
-  puts "<tr><td>Cookie:</td><td>%s</td></tr>\n" % ck
+
+cgi = CGI.new("html4")
+cookie = cgi.cookies['name']
+ck = cookie[0]
+puts "<tr><td>Cookie:</td><td>%s</td></tr>" % ck
+'''
+#ck = (ENV["HTTP_COOKIE"].split(";"))[1]
+#ck = ck.slice(1..(ck.length-1))
+if ck.length > 0
+  if ck != "destroyed"
+    puts "<tr><td>Cookie:</td><td>%s</td></tr>\n" % ck
+  end
 else
   puts "<tr><td>Cookie:</td><td>None</td></tr>\n"
-
+end
+'''
 puts "</table>"
   
 # Links for other pages
 puts "<br />"
-puts "<a href=\"/cgi-bin/py-sessions-1.py\">Session Page 1</a>"
+puts "<a href=\"/cgi-bin/ruby-sessions-1.rb\">Session Page 1</a>"
 puts "<br />"
-puts "<a href=\"/py-cgiform.html\">PY CGI Form</a>"
+puts "<a href=\"/ruby-cgiform.html\">Ruby CGI Form</a>"
 puts "<br /><br />"
 
 # Destroy Cookie button
-puts "<form action=\"/cgi-bin/py-destroy-session.py\" method=\"get\">"
+puts "<form action=\"/cgi-bin/ruby-destroy-session.py\" method=\"get\">"
 puts "<button type=\"submit\">Destroy Session</button>"
 puts "</form>"
 puts "</body>"
